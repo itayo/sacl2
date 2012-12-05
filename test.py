11ff33051e1c9ce73db5429e5127aa1f95038450
Stefan Nygren <stefan.nygren@gmail.com>
2012-12-05 23:39:20 +0100

    fix test

--- test.py
+++ test.py
@@ -4,11 +4,5 @@ class TestSequenceOne(unittest.TestCase):
   def test_Success(self):
     self.assertEqual('a','a')
     self.assertTrue(True)
-  def test_FailOne(self):
-    self.assertEqual('a','b')
-  def test_FailTwo(self):
-    self.assertTrue(False)
-  def test_FailThree(self):
-    self.assertRaises(TypeError,dir,unittest)
 if __name__ == '__main__':
   unittest.main()
