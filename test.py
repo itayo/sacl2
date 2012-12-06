4595b518f39131051a9d68a062d8ebe1001a4d69
Stefan Nygren <Stefan.Nygren@maquet.com>
2012-12-06 10:28:42 +0100

    Showing Travis

--- test.py
+++ test.py
@@ -4,5 +4,7 @@ class TestSequenceOne(unittest.TestCase):
   def test_Success(self):
     self.assertEqual('a','a')
     self.assertTrue(True)
+  def test_Fail(self):
+    self.assertTrue(False)
 if __name__ == '__main__':
   unittest.main()
