360f2c49a4a7748f2829c018c6466346e5919590
Stefan Nygren <Stefan.Nygren@maquet.com>
2012-12-06 10:31:04 +0100

    removing failing test

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ class TestSequenceOne(unittest.TestCase):
   def test_Success(self):
     self.assertEqual('a','a')
     self.assertTrue(True)
-  def test_Fail(self):
-    self.assertTrue(False)
+
 if __name__ == '__main__':
   unittest.main()
