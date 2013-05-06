772097054c22cee29b52d93d39535406812caa5d
root <root@onegai.stny.se>
2013-05-06 18:14:14 +0400

    coverage report now works :)

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
-coverage run test.py
+python test.py
 coverage run sacl2.py
-coverage report *.py
-coverage annotate *.py
-cat *cover
+coverage annotate sacl2.py
+cat sacl2.py,cover
+python test.py
+coverage report sacl2.py
